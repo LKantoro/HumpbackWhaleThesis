db576a587459ab34a392acf8f573428cba36fe86
Lucas Kantorowski <lkantoro@calpoly.edu>
2026-03-23 21:42:16 -0700

    started custom denominator

--- CustomDenominator.R
+++ CustomDenominator.R
@@ -7,6 +7,65 @@
 #How to judge incorrect predictions using this metric?
   
   
+# take all true annotations
+# round both ends to nearest 0.05
+# determine the percentage of positive predictions out of segments that overlap it
+# record that metric and compare it to a threshold
+
+
+
+# input contains: (ldapredictions, manualannotations)
+# time_start
+# time_end
+# prediction (0/1)
+# truth (0/1)
+# length of rolling evaluation windows
+
+
+manual_annotations = read_excel(here::here("TrainingData", "AW", "AWhb.xlsx"))
+
+manual_annotations = manual_annotations %>% rename(Begin_Time = 'Begin Time (s)',
+                                 End_Time = 'End Time (s)')
+
+
+custom_denominator = function(){
+  # pseudocode:
+  outputdf = data.frame()
+  # for each annotated range
+  for (i in c(1:nrow(manual_annotations))){
+    #print(i)
+    
+    start_time_annotated = manual_annotations$Begin_Time[i]
+    end_time_annotated = manual_annotations$End_Time[i]
+    
+    
+    proportion = ldapredictions %>% filter(Time_Start < end_time_annotated &
+                                start_time_annotated < Time_End) %>% 
+      summarise(proportion = sum(preds == 1) / n()) %>% pull(proportion)
+    
+    prediction = as.numeric(proportion > threshold)
+    
+    row = c("Time_Start" = i, 
+            "Time_End" = i + length, 
+            "proportion" = proportion, 
+            "prediction" = prediction)
+  }
+  colnames(outputdf) = c("Time_Start", "Time_End", "proportion", "prediction")
+  
+}
+
+  # if beginning of 0.1 second window is less than end of annotated window
+  # and if end of 0.1 window is greater than beginning of annotated window
+    # investigate whether prediction is 0 or 1
+  # calculate the proportion of 1s
+  # add row with start_time, end_time, and proportion to output
+# output 
+
+
+
+
+
+
 annotatedAW = read_excel(here::here("TrainingData", "AW", "AWhb.xlsx"))
 
 
@@ -18,5 +77,6 @@ annotatedAW = annotatedAW %>% dplyr::rename("begin_time" = "Begin Time (s)",
 
 
 annotatedAW
+
 
 
