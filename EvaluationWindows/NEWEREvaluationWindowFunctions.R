ed05830694964ca911a904f740a6bee8da3c4182
Lucas Kantorowski <lkantoro@calpoly.edu>
2026-05-01 19:57:12 -0700

    deleted innefficient funtions, renamed new to old

--- EvaluationWindows/NEWEREvaluationWindowFunctions.R
+++ EvaluationWindows/NEWEREvaluationWindowFunctions.R
@@ -1,6 +1,4 @@
-testing_eval_windows_linear = function(predictions, length, threshold) {
-  
- 
+testing_eval_windows_linear_base = function(predictions, length) {
   
   n = nrow(predictions)
   if (n == 0) {
@@ -8,8 +6,7 @@ testing_eval_windows_linear = function(predictions, length, threshold) {
       Time_Start = numeric(0),
       Time_End = numeric(0),
       proportion = numeric(0),
-      prediction = factor(),
-      Humpback = factor()
+      Humpback = integer(0)
     ))
   }
   
@@ -35,16 +32,13 @@ testing_eval_windows_linear = function(predictions, length, threshold) {
       break
     }
     
-    # Expand right edge: include rows with Time_Start < window_end
     while (j <= n && time_start[j] < window_end) {
       count_preds1 = count_preds1 + pred_flag[j]
       count_whales = count_whales + whale_flag[j]
       j = j + 1L
     }
     
-    # Current window is rows k:(j-1)
     window_size = j - k
-    
     proportion = if (window_size > 0L) count_preds1 / window_size else NA_real_
     humpback_present = as.integer(count_whales >= 1L)
     
@@ -53,24 +47,14 @@ testing_eval_windows_linear = function(predictions, length, threshold) {
       Time_Start = i,
       Time_End = window_end,
       proportion = proportion,
-      prediction = NA,
       Humpback = humpback_present
     )
     
-    # Before moving left edge from k to k+1, remove row k if it is in the window
     if (k < j) {
       count_preds1 = count_preds1 - pred_flag[k]
       count_whales = count_whales - whale_flag[k]
     }
   }
   
-  results = do.call(rbind, rows[seq_len(iter)])
-  
-  results = results %>%
-    dplyr::mutate(
-      prediction = as.factor(as.integer(proportion >= threshold)),
-      Humpback = as.factor(Humpback)
-    )
-  
-  return(results)
+  do.call(rbind, rows[seq_len(iter)])
 }
